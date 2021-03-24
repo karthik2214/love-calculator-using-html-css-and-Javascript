632bb5adaa3c68549bdfe53017b403249408b134
karthik2214 <71806384+karthik2214@users.noreply.github.com>
2021-03-24 09:15:01 +0000

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,30 @@
+function result() {
+  
+  let name = document.getElementById("name").value;
+  let lname = document.getElementById("lname").value;
+  
+  if (name === "") {
+    alert('Please enter your name');
+  }else if (name.length <= 2) {
+    alert('Ohree or more than three characters are allowed');
+  }else if (!isNaN(name)) {
+    alert('Only letters are allowed');
+  }
+  
+  else if (name === "") {
+    alert('Please enter your name');
+  }else if (name.length <= 2) {
+    alert('Three or more than three characters are allowed');
+  }else if (!isNaN(name)) {
+    alert('Only letters are allowed');
+  }
+  
+  else{
+  let data = Math.random() * 100;
+  data = Math.floor(data);
+  document.getElementById("value").value = data + "%";
+
+  }
+  
+  
+}
